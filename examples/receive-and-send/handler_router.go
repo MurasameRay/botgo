bfc999d79372483b88565d7fb8b51e2753b1f21d
MurasameRay <1353577267@qq.com>
2024-12-16 19:37:26 +0800

    test

--- examples/receive-and-send/handler_router.go
+++ examples/receive-and-send/handler_router.go
@@ -33,8 +33,10 @@ var CommandRegistry = []struct {
 }
 
 func imageHandler(input string) string {
-	folderPath := "/www/wwwroot/blog.xiaocongyu.com/wp-content/uploads" // 替换为您的文件夹路径
-	urlPrefix := "http://blog.xiaocongyu.com/wp-content/uploads"
+	//folderPath := "/www/wwwroot/blog.xiaocongyu.com/wp-content/uploads" // 替换为您的文件夹路径
+	folderPath := "/root/images" // 替换为您的文件夹路径
+	//urlPrefix := "http://blog.xiaocongyu.com/wp-content/uploads"
+	urlPrefix := "http://blog.xiaocongyu.com"
 	if strings.Contains(input, "ba") || strings.Contains(input, "blue archive") ||
 		strings.Contains(input, "blue") || strings.Contains(input, "档案") {
 		folderPath += "/blue_archive"
